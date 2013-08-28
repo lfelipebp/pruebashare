0b45839a6b4ed0f68f8fa01c4cfedfc11a85be50
LFBP <felipebejaranop@gmail.com>
2013-08-28 09:38:59 -0500

    Primer upload

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Pruebashare::Application.config.secret_key_base = '69bbb0ca3ba35d7e17a691c65542d96188ed67978fa5578e45969c225c1709c061c068f038c34bc5be703daa52e3f3a3fd5d6ac266bf08a2a20e26d459630a90'
---
+++ pruebashare/config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Pruebashare::Application.config.secret_key_base = '793a6b239ad87052f488a6eed2b78d92223fa99fdca8e80c41d12ad4d8e6916148a018bc0c347dd2117ecc52cca1f79a32613683f4e970c6166161ebb7afc36e'
